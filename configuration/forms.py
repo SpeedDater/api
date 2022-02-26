a100aeb5a4d3dc430702ae61568657c19504a6c6
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2022-02-26 18:59:02 -0500

    tweak admin ui

--- configuration/forms.py
+++ configuration/forms.py
@@ -2,10 +2,11 @@ from django import forms
 
 
 class AddStaffForm(forms.Form):
-	email = forms.EmailField(label="Email address")
-	first_name = forms.CharField()
-	last_name = forms.CharField()
+    email = forms.EmailField(label="Email address")
+    first_name = forms.CharField()
+    last_name = forms.CharField()
 
 
 class BulkUpdateForm(forms.Form):
-	content = forms.CharField(widget=forms.Textarea, label="Skills (enter one per line)")
+    content = forms.CharField(widget=forms.Textarea,
+                              label="Skills (enter one per line)")
--- configuration/views.py
+++ configuration/views.py
@@ -11,39 +11,39 @@ from configuration.serializers import *
 
 
 class MajorViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows majors to be viewed or edited.
-    """
-    queryset = Major.objects.all().order_by('major')
-    serializer_class = MajorSerializer
-    permission_classes = [permissions.IsAdminOrReadOnly]
+	"""
+	API endpoint that allows majors to be viewed or edited.
+	"""
+	queryset = Major.objects.all().order_by('major')
+	serializer_class = MajorSerializer
+	permission_classes = [permissions.IsAdminOrReadOnly]
 
 
 class SectionTimeViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows section times to be viewed or edited.
-    """
-    queryset = SectionTime.objects.all().order_by('start')
-    serializer_class = SectionTimeSerializer
-    permission_classes = [permissions.IsAdminOrReadOnly]
+	"""
+	API endpoint that allows section times to be viewed or edited.
+	"""
+	queryset = SectionTime.objects.all().order_by('start')
+	serializer_class = SectionTimeSerializer
+	permission_classes = [permissions.IsAdminOrReadOnly]
 
 
 class SkillViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows section times to be viewed or edited.
-    """
-    queryset = Skill.objects.all().order_by('skill')
-    serializer_class = SkillSerializer
-    permission_classes = [permissions.IsAdminOrReadOnly]
+	"""
+	API endpoint that allows section times to be viewed or edited.
+	"""
+	queryset = Skill.objects.all().order_by('skill')
+	serializer_class = SkillSerializer
+	permission_classes = [permissions.IsAdminOrReadOnly]
 
 
 class UserViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows Users to be viewed or edited.
-    """
-    queryset = User.objects.all().order_by('last_name', 'first_name')
-    serializer_class = UserSerializer
-    permission_classes = [permissions.IsAuthenticatedAndReadOnly]
+	"""
+	API endpoint that allows Users to be viewed or edited.
+	"""
+	queryset = User.objects.all().order_by('last_name', 'first_name')
+	serializer_class = UserSerializer
+	permission_classes = [permissions.IsAuthenticatedAndReadOnly]
 
 
 @method_decorator(staff_member_required(), name='dispatch')
@@ -76,35 +76,35 @@ class AddStaffView(FormView):
 
 @method_decorator(staff_member_required(), name='dispatch')
 class MajorBulkUpdateView(FormView):
-    """
-    A view for admins to bulk import majors.
-    """
-    form_class = BulkUpdateForm
-    success_url = reverse_lazy('admin:configuration_major_changelist')
-    template_name = 'bulk_import_majors.html'
+	"""
+	A view for admins to bulk import majors.
+	"""
+	form_class = BulkUpdateForm
+	success_url = reverse_lazy('admin:configuration_major_changelist')
+	template_name = 'bulk_import_majors.html'
 
-    def form_valid(self, form):
-        # load input per line
-        majors = form.cleaned_data['content'].split('\r\n')
-        # build objects list, bulk create in db, ignore non-unique objects
-        objs = [Major(major=m) for m in majors]
-        Major.objects.bulk_create(objs, ignore_conflicts=True)
-        return super().form_valid(form)
+	def form_valid(self, form):
+		# load input per line
+		majors = form.cleaned_data['content'].split('\r\n')
+		# build objects list, bulk create in db, ignore non-unique objects
+		objs = [Major(major=m) for m in majors]
+		Major.objects.bulk_create(objs, ignore_conflicts=True)
+		return super().form_valid(form)
 
 
 @method_decorator(staff_member_required(), name='dispatch')
 class SkillBulkUpdateView(FormView):
-    """
-    A view for admins to bulk import skills.
-    """
-    form_class = BulkUpdateForm
-    success_url = reverse_lazy('admin:configuration_skill_changelist')
-    template_name = 'bulk_import_skills.html'
+	"""
+	A view for admins to bulk import skills.
+	"""
+	form_class = BulkUpdateForm
+	success_url = reverse_lazy('admin:configuration_skill_changelist')
+	template_name = 'bulk_import_skills.html'
 
-    def form_valid(self, form):
-        # load input per line
-        skills = form.cleaned_data['content'].split('\r\n')
-        # build objects list, bulk create in db, ignore non-unique objects
-        objs = [Skill(skill=s) for s in skills]
-        Skill.objects.bulk_create(objs, ignore_conflicts=True)
-        return super().form_valid(form)
+	def form_valid(self, form):
+		# load input per line
+		skills = form.cleaned_data['content'].split('\r\n')
+		# build objects list, bulk create in db, ignore non-unique objects
+		objs = [Skill(skill=s) for s in skills]
+		Skill.objects.bulk_create(objs, ignore_conflicts=True)
+		return super().form_valid(form)
