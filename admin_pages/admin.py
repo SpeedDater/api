5ced4ca99a0d5ac55a9ec237a3aba4cc10313a77
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2022-03-05 22:44:44 -0500

    disable admin modules for allauth

--- admin_pages/admin.py
+++ admin_pages/admin.py
@@ -1,15 +1,24 @@
+from allauth.socialaccount.admin import SocialAccount, SocialApp, SocialToken
 from django.conf import settings
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group, User
+from django.contrib.sites.models import Site
 
 # set admin site name
 admin.site.site_header = settings.APP_NAME
 admin.site.site_title = settings.APP_NAME
 admin.site.index_title = 'Admin Panel'
+# disable Django user admin (replaced with custom admin with actions)
+admin.site.unregister(User)
 # disable Django groups
 admin.site.unregister(Group)
-admin.site.unregister(User)
+# disable Django sites
+admin.site.unregister(Site)
+# disable allauth social account modules
+admin.site.unregister(SocialAccount)
+admin.site.unregister(SocialApp)
+admin.site.unregister(SocialToken)
 
 
 @admin.register(User)
