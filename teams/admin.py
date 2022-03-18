9300df6feb510b625c00f2ff25c8fa3ceae0e2c8
Chris Xiao <30990835+chrisx8@users.noreply.github.com>
2022-03-18 14:31:14 -0400

    actually validate teams
    * holy **** that took forever

--- teams/admin.py
+++ teams/admin.py
@@ -4,24 +4,24 @@ from teams.models import Team
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
-    autocomplete_fields = ['members', 'availability']
+    autocomplete_fields = ['member1', 'member2', 'member3', 'member4',
+                           'member5', 'availability']
     list_display = ['__str__', 'assigned_section', 'preferred_section']
     list_filter = ['availability', 'preferred_section', 'assigned_section']
     ordering = ['number']
     readonly_fields = ['number']
-    search_fields = ['number', 'members__username', 'members__email',
-                     'members__first_name', 'members__last_name']
+    search_fields = ['number']
     fieldsets = [
         ('Team Number', {
             'fields': ['number']
         }),
         ('Members', {
-            'fields': ['members']
+            'fields': ['member1', 'member2', 'member3', 'member4', 'member5']
         }),
         ('Availability', {
-            'fields': ['availability', 'preferred_section'],
+            'fields': ['availability', 'preferred_section']
         }),
         ('Assignment', {
-            'fields': ['assigned_section'],
+            'fields': ['assigned_section']
         }),
     ]
--- teams/models.py
+++ teams/models.py
@@ -2,11 +2,21 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.db import models
 from configuration.models import Section, SectionTime
+from django.core.validators import MinValueValidator, MinLengthValidator
 
 
 class Team(models.Model):
     number = models.SmallIntegerField(null=True, verbose_name='Team number')
-    members = models.ManyToManyField(User)
+    member1 = models.OneToOneField(User, on_delete=models.CASCADE,
+                                   related_name='member1', blank=True, null=True)
+    member2 = models.OneToOneField(User, on_delete=models.CASCADE,
+                                   related_name='member2', blank=True, null=True)
+    member3 = models.OneToOneField(User, on_delete=models.CASCADE,
+                                   related_name='member3', blank=True, null=True)
+    member4 = models.OneToOneField(User, on_delete=models.CASCADE,
+                                   related_name='member4', blank=True, null=True)
+    member5 = models.OneToOneField(User, on_delete=models.CASCADE,
+                                   related_name='member5', blank=True, null=True)
     availability = models.ManyToManyField(SectionTime)
     preferred_section = models.ForeignKey(Section, on_delete=models.CASCADE,
                                           related_name='preferred_section')
@@ -14,24 +24,52 @@ class Team(models.Model):
                                          null=True, blank=True,
                                          related_name='assigned_section')
 
+    def count_members(self):
+        # team is not full
+        if None in members_set:
+            return len(members) - 1
+        else:
+            return len(members)
+
+    def clean(self):
+        count = 0
+        members = [self.member1, self.member2,
+                   self.member3, self.member4, self.member5]
+        members_set = set(members)
+
+        # count members in list
+        for m in members:
+            if m:
+                count += 1
+        # count members in set (subtract one if there are null values)
+        set_count = (len(members_set) - 1) if None in members_set \
+                                           else len(members_set)
+
+        # if two counts are different, there are duplicates
+        if count != set_count:
+            raise ValidationError('Teams must not have duplicate members.')
+        # check min/max count
+        if count < 2:
+            raise ValidationError('Teams must have at least 2 members.')
+        elif count > 5:
+            raise ValidationError('Teams may only have up to 5 members')
+
     def save(self, *args, **kwargs):
-        # TODO: FIX ADMIN CRASH
-        # this validates, but failed validation would just crash the admin panel
+        # validate team
+        self.clean()
 
-        # limit members to 2 or more
-        if (self.members.count() < 2):
-            raise ValidationError('A team must have at least 2 members.')
-        # limit members to 5 or less
-        if (self.members.count() > 5):
-            raise ValidationError('A team many only have up to 5 members.')
-
-        # change Team ID to section + # in section (ex. 407)
+        # change Team Number to section + # in section (ex. 407)
         # only assign number if team is assigned to section and there isn't a number yet
         if self.assigned_section and not self.number:
             num_in_section = Team.objects.filter(
                 assigned_section=self.assigned_section).count() + 1
             num_in_section = str(num_in_section).zfill(2)
             self.number = int(f'{self.assigned_section}{num_in_section}')
+        # remove Team Number if team becomes unassigned
+        elif not self.assigned_section and self.number:
+            self.number = None
+
+        # actually save
         super().save(*args, **kwargs)
 
     def __str__(self):
